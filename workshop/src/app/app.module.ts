b5d82967c1d1600ea458c6161250f1cddd878f26
AndonGeorgiev <andon2110@gmail.com>
2022-03-20 19:03:21 +0200

    add app-header html structure

--- workshop/src/app/app.module.ts
+++ workshop/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppHeaderComponent } from './core/app-header/app-header.component';
 
 @NgModule({
   declarations: [
