45cc562192803dde4ea69c1cdc6218ca5ae3defa
AndonGeorgiev <andon2110@gmail.com>
2022-03-20 22:11:02 +0200

    change header dir

--- workshop/src/app/core/core.module.ts
+++ workshop/src/app/core/core.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './app-header/app-header.component';
+import { HeaderComponent } from './header/app-header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AsideComponent } from './aside/aside.component';
 
