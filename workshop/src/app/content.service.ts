0a1913dec7cafd3459e2a9cd182b8a3c19feb2e3
AndonGeorgiev <andon2110@gmail.com>
2022-03-24 22:16:04 +0200

    add content service

---
+++ workshop/src/app/content.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { IPost } from './shared/interfaces/post';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ContentService {
+
+
+}
