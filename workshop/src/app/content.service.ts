d6010f74447de0a524c7aa114b5064f1cfc5050b
AndonGeorgiev <andon2110@gmail.com>
2022-03-24 22:16:37 +0200

    add get post functionality

--- workshop/src/app/content.service.ts
+++ workshop/src/app/content.service.ts
@@ -7,5 +7,11 @@ import { Observable } from 'rxjs';
 })
 export class ContentService {
 
+  constructor(private httpService: HttpClient) { }
 
+  getPosts$(): Observable<IPost[]> {
+    return this.httpService.get<IPost[]>('http://localhost:3000/api/posts', {
+      params: new HttpParams({ fromObject: { limit: 5 } })
+    });
+  }
 }
