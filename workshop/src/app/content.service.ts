2464567d735ad2d1b2e737155b245be06fe3a11c
AndonGeorgiev <andon2110@gmail.com>
2022-03-25 08:42:19 +0200

    add get theme fun

--- workshop/src/app/content.service.ts
+++ workshop/src/app/content.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { IPost } from './shared/interfaces/post';
 import { Observable } from 'rxjs';
+import { ITheme } from './shared/interfaces/theme';
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
 
   constructor(private httpService: HttpClient) { }
+
+  getThemes$(): Observable<ITheme[]>{
+    return this.httpService.get<ITheme[]>('http://localhost:3000/api/themes');
+  }
 
   getPosts$(): Observable<IPost[]> {
     return this.httpService.get<IPost[]>('http://localhost:3000/api/posts?limit=5');
