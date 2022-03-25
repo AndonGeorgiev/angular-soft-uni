3dc462a0ac1ae4b4731e028b3e357744661823f1
AndonGeorgiev <andon2110@gmail.com>
2022-03-25 08:39:52 +0200

    add theme component

---
+++ workshop/src/app/theme/theme.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-theme',
+  templateUrl: './theme.component.html',
+  styleUrls: ['./theme.component.css']
+})
+export class ThemeComponent implements OnInit {
+
+
+}
