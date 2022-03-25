e07070037cb347670f8c378974036b2d9560f28a
AndonGeorgiev <andon2110@gmail.com>
2022-03-25 08:41:39 +0200

    add theme functionality and html structure

--- workshop/src/app/theme/theme.component.ts
+++ workshop/src/app/theme/theme.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { ITheme } from '../shared/interfaces/theme';
+import{ContentService} from './../content.service'
 
 @Component({
   selector: 'app-theme',
@@ -7,5 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThemeComponent implements OnInit {
 
+  themes: ITheme[] = [];
+  constructor(private contentService : ContentService) { }
+
+  ngOnInit(): void {
+    this.contentService.getThemes$()
+    .pipe(
+      tap(themes => this.themes = themes)
+    )
+    .subscribe();
+  }
 
 }
